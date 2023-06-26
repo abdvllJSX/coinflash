4851cb7198b08c5199434e37675235e0696680f6
abdvlljsx <abdvlljsx@gmail.com>
2023-06-26 12:47:07 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Home from './components/home/home';
 function App() {
-  const [count, setCount] = useState(0)
+  const [coins, setCoins] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en'
 
+  useEffect(() => {
+    fetch(url).then(response => response.json()).then(data => setCoins(data))
+
+  }, [])
   return (
     <>
+    <Home coins ={coins}/>
     </>
   )
 }
